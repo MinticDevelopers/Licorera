0f3e767e29ed5a3ec869d9a04a8ec7dc124fe771
edwinri0s <edwinrios490@gmail.com>
2022-10-12 21:47:24 -0500

    Configuracion del ENV
    Se crean los archivos server.js, app.js y config.env, se genera una respuesta en consola para verificar la conexión del servidor

---
+++ back/server.js
@@ -0,0 +1,10 @@
+const app = require("./app")
+
+//Establecer el archivo de configuración
+const dotenv = require("dotenv");
+dotenv.config({path: 'back/config/config.env'})
+
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Servidor iniciado en el puerto: ${process.env.PORT} en modo: ${process.env.NODE_ENV}`)
+})
