34832ebc08d4aab0d92bf0fa1f8a3b9f9fb3e5e0
edwinri0s <edwinrios490@gmail.com>
2022-10-19 00:12:30 -0500

    Se crean los metodos de CRUD
    Se crean los metodos de Consultar, Actualizar y Eliminar productos y sus respectivas rutas.

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,12 +1,32 @@
 const producto = require ("../models/productos")//importación del schema productos
 
 //Ver la lista de productos
-exports.getProducts=(req,res,next) =>{
+exports.getProducts=async (req,res,next) =>{
+    const productos= await producto.find();
     res.status(200).json({
         success:true,
-        message: "En esta ruta usted podra ver todos los productos"
+        count: productos.length,
+        productos
     })
 }
+
+//Ver un producto por ID
+exports.getProductById= async (req, res, next)=>{
+    const product= await producto.findById(req.params.id)
+    
+    if (!product){
+            return res.status(404).json({
+            success:false,
+            message: 'No se encontro el producto con el ID especificado'
+        })
+    }
+    res.status(200).json({
+        success:true,
+        message:"A continuación encuentras información sobre tu producto: ",
+        product
+    })
+}
+
 
 //Crear nuevo producto /api/productos
 exports.newProduct=async(req,res,next)=>{
@@ -17,3 +37,53 @@ exports.newProduct=async(req,res,next)=>{
         product
     })
 }
+
+//Actualizar un producto
+exports.updateProduct= async (req,res,next) =>{
+    // Se crea una variable modificable
+    let product= await producto.findById(req.params.id) 
+    //Se verifica que el producto no existe para finalizar el proceso
+    if (!product){
+            return res.status(404).json({
+            success:false,
+            message: 'No se encontro el producto con el ID especificado'
+        })
+    }
+
+    //Si el producto ya existia se actualizan los datos
+    product= await producto.findByIdAndUpdate(req.params.id, req.body, {
+        //Se validan los atributos nuevos
+        new:true,
+        runValidators:true
+    });
+
+    //Se da una respuesta positiva si se pudo actualizar los datos
+    res.status(200).json({
+        success:true,
+        message:"Producto actualizado correctamente",
+        product
+    })
+}
+
+//Eliminar un producto
+exports.deleteProduct= async (req,res,next) =>{
+    // Se crea una variable modificable
+    const product= await producto.findById(req.params.id)
+    
+    //Se verifica que el producto no existe para finalizar el proceso
+    if (!product){ 
+            //Si el objeto no existe, return termina el metodo
+            return res.status(404).json({ 
+            success:false,
+            message: 'No se encontro el producto con el ID especificado'
+        })
+    }
+
+    // Si no se encontro el Id especificado se rompe la ejecucion y se salta el método
+    await product.remove();
+    res.status(200).json({
+        success:true,
+        message:"Producto eliminado correctamente"
+    })
+}
+
--- back/routes/products.js
+++ back/routes/products.js
@@ -2,12 +2,23 @@ const express=require("express")
 const router=express.Router();
 
 //Se importa la respuesta Json desde el controlador
-const {getProducts, newProduct} = require("../controllers/productsController") 
+const {getProducts, newProduct, getProductById, updateProduct, deleteProduct} = require("../controllers/productsController") 
 
-//Se establece en que ruta se ven los productos (getProducts)
-router.route('/productos').get(getProducts)
-//Establecemos desde que ruta queremos ver el postProductos
+//Se establece en que ruta se ven los productos (getProducts).
+router.route('/productos').get(getProducts);
+
+//Establecemos desde que ruta queremos ver el postProductos.
 router.route('/producto/nuevo').post(newProduct); 
+
+//Se establece la ruta para buscar producto por ID.
+router.route('/producto/:id').get(getProductById);
+
+// Se establece la ruta para actualizar los productos.
+router.route('/producto/:id').put(updateProduct);
+
+//Se establece la ruta para eliminar un producto
+router.route('/producto/:id').delete(deleteProduct);
 
 //Se exporta el modelo.
 module.exports=router;
+
