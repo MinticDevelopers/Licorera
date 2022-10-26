8703ad9f164b823f0d7303b449d3adc76c7bb4ab
edwinri0s <edwinrios490@gmail.com>
2022-10-26 00:22:23 -0500

    Se inicia la creacion del front
    Se inicia la creacion del front

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Tienda Virtual Liqueurs Styles</h1>
+     <img src="./images/logo.png" alt="Aqui va el logo"></img>
+    </div>
+  );
+}
+
+export default App;
